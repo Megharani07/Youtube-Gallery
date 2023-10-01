30b8e4a5ee95e8b6f40385f8f2aae4805ef434c8
Megharani07 <143949076+Megharani07@users.noreply.github.com>
2023-10-01 23:57:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <header>
+      <a>All</a>
+      <a>Music</a>
+      <a>Home</a>
+      <a>Videos</a>
+      <a>Playlist</a>
+      </header>
+
+      <div class="logo-item">
+        <img id="logo" width="75px"  src="https://yt3.googleusercontent.com/ytc/APkrFKZVYOepe_d6tmkh_kHVsw9hbZD-nFPABWHnc6cgUw=s176-c-k-c0x00ffffff-no-rj-mo"/>
+      </div>
+      </div>
+  )
+}
+
+export default App;

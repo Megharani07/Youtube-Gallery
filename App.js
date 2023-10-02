e9db067d0cc182d108b7964accad9761a8963c37
Megharani07 <143949076+Megharani07@users.noreply.github.com>
2023-10-02 15:22:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import './videodata.css';
+import GalleryData from './GalleryData';
 
 function App() {
   return(
@@ -12,9 +14,13 @@ function App() {
       <a>Playlist</a>
       </header>
 
-      <div class="logo-item">
-        <img id="logo" width="75px"  src="https://yt3.googleusercontent.com/ytc/APkrFKZVYOepe_d6tmkh_kHVsw9hbZD-nFPABWHnc6cgUw=s176-c-k-c0x00ffffff-no-rj-mo"/>
-      </div>
+      <div className="logo-item">
+        <img className="logo" width="90px"  src="https://yt3.googleusercontent.com/ytc/APkrFKZVYOepe_d6tmkh_kHVsw9hbZD-nFPABWHnc6cgUw=s176-c-k-c0x00ffffff-no-rj-mo"/>
+        <h2 className="data">Ashish Chanchlani Vines
+        <p>@ashishchanchlanivines</p></h2>
+        </div>
+
+        <GalleryData/>
       </div>
   )
 }
